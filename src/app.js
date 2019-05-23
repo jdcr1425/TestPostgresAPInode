893b5f571ba3d217245f288c8db9dd09953aa2ef
jdcr1425 <juandavidcr7@gmail.com>
2019-05-22 21:49:35 -0500

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express, {json} from 'express';
+
+const app = express();
+
+
+//importing routes
+import projectRoutes from './routes/projects';
+import tasksRoutes from './routes/projects';
+
+//
+app.use(morgan('dev'));
+app.use(json());
+
+//Routes
+
+app.use('/api/projects',projectRoutes);
+app.use('/api/tasks',tasksRoutes);
+
+export default  app;
